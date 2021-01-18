484fadb8ac804b8529cd75011019ff25948e7315
Jake Fullmer <w.jake.fullmer@gmail.com>
2021-01-17 18:09:14 -0700

    added reactionSchema to Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,32 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+},
+{
+    toJSON: {
+        getters: true
+    },
+    id: false
+});
 
 const thoughtSchema = new Schema(
     {
